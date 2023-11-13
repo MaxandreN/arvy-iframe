0dcadb027d162d8641ad91c5792055ae796153ee
Max <Maxandre.neveux@gmail.com>
2023-11-13 16:14:08 +0100

    Initial commit

---
+++ arvy-card.js
@@ -0,0 +1,104 @@
+import loaclDatas from './src/local_datas/wods.json'
+import style from './build/style.css'
+const template = document.createElement('template');
+
+template.innerHTML = `
+  <style>
+    ${style}
+  </style>
+  <div class="arvy">
+    <h1 class="arvy-title">Nos wods de CrossFit par Arvy</h1>
+    <div class="arvy-wods"></div>
+  </div>
+`
+
+class ArvyCard extends HTMLElement {
+    constructor() {
+      super();
+      this._shadowRoot = this.attachShadow({ 'mode': 'open' });
+      this._shadowRoot.appendChild(template.content.cloneNode(true));
+      this.allowDisplay = ["equipment","exercises","trainerTips"];
+      this.allowDisplayWording = 
+        {
+          "equipment":"Équipments",
+          "exercises":"Exercices",
+          "trainerTips":"Conseils d'entraîneur"
+        };
+    }
+
+    get isLocal() {
+        return this.getAttribute('isLocal');
+    }
+      
+    get pathData() {
+        return this.getAttribute('pathData');
+    }
+          
+    get apiKey() {
+        return this.getAttribute('apiKey');
+    }
+
+    getCardBody(list) {
+      const htmlCardBody = document.createElement('div');
+      htmlCardBody.classList = 'arvy-wod-body';
+
+      this.allowDisplay.forEach(elementShow => {
+
+        if(list[elementShow].length > 0){
+          const htmlContainer = document.createElement('div');
+          htmlContainer.classList = '.arvy-wod-'+elementShow;
+  
+          const title = document.createElement('h3');
+          title.innerText = this.allowDisplayWording[elementShow];
+          htmlContainer.appendChild(title);
+          
+          htmlContainer.appendChild(this.getElementContent(list[elementShow]));
+
+          htmlCardBody.appendChild(htmlContainer);
+        }
+      });
+      return htmlCardBody;
+    }
+
+    getElementContent(list){
+      let htmlListContainer = document.createElement('ul');
+      list.forEach(value => {
+        let htmlListElement = document.createElement('li')
+        htmlListElement.innerText = value;
+
+        htmlListContainer.appendChild(htmlListElement);
+      })
+      return htmlListContainer;
+    }
+      
+    connectedCallback() {
+        let dataset = "";
+        if(!this.isLocal && this.apiKey !== null ){
+            var xmlHttp = new XMLHttpRequest();
+            const url = `http://arvy.app/api/workflow?key=${this.apiKey}`
+            xmlHttp.open("GET", url, false);
+            xmlHttp.send(null);
+            dataset = JSON.parse(xmlHttp.responseText);
+        }else{            
+            dataset = loaclDatas;
+        }
+
+        this.$containt = this._shadowRoot.querySelector('.arvy-wods');
+        dataset.workouts.forEach(wod => {
+            console.log(wod);
+            let wodCard = document.createElement('div');
+            wodCard.classList = "arvy-wod-card"
+            wodCard.innerHTML = `
+              <div class="arvy-wod-head">
+                <h2 class="arvy-wod-title">${wod.name}</h2>
+                <p class="arvy-wod-mode">${wod.mode}</p>
+              </div>
+            `
+            wodCard.appendChild(this.getCardBody(wod));
+
+            this.$containt.appendChild(wodCard);
+        });
+    }
+  }
+
+  window.customElements.define('arvy-card', ArvyCard);
